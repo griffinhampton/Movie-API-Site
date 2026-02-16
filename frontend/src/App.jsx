fd63f632b1a008552ae15801c3bc9125eb2891b4
Griffin Hampton <gmhamp01@louisville.edu>
2026-02-16 16:23:41 -0500

    final-commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import './css/App.css'
+import MovieCard from './components/MovieCard';
+import Favorites from './pages/Favorites';
+import Home from "./pages/Home"
+import {Routes, Route} from "react-router-dom"
+import NavBar from "./components/NavBar"
+import { MovieProvider } from './contexts/MovieContext';
+
+function App() {
+
+  const movieNumber = 1;
+
+  return (
+    <div>
+      <MovieProvider>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />}/>
+        </Routes>
+      </main>
+      </MovieProvider>
+    </div>
+);
+}
+
+
+export default App
